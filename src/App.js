55d82d0450f5f1cd41c6fec21b3d49c2d1eaf6bb
Vid Rotar <vidrotar@Vids-MacBook-Pro-2.local>
2023-01-25 16:03:15 +0100

    fix react-ga library versions

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import Experience from "./sections/experience/Experience.tsx";
 import Contact from "./sections/contact/Contact.tsx";
 import Header from "./sections/header/Header.tsx";
 import {ScrollingProvider} from "react-scroll-section";
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 
 const TRACKING_ID = "G-S2QGL3QDFR"; // YOUR_OWN_TRACKING_ID
 
 function App() {
   ReactGA.initialize(TRACKING_ID);
-  ReactGA.pageview(window.location.pathname + window.location.search);
+  ReactGA.send("pageview");
 
 
   return (
